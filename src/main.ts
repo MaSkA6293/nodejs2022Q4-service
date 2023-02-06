ed41da6b3852c15a393e2b36f4edd47eadd8d73e
Oleg Shkalikov <maska6293@yandex.ru>
2023-02-06 11:03:47 +0300

    fix: enable CORS, correct link in api.yaml

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const port = process.env.PORT ? process.env.PORT : 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
