09eff54d07150acb9c968bed5d7a6235abb47cba
Oleg Shkalikov <maska6293@yandex.ru>
2023-02-20 11:12:26 +0300

    feat: implement listener and logging to uncaughtException, unhandledRejection events

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { CustomExceptionFilter } from './middlewares/exceptionFilter/exceptionFilter.service';
+import * as fs from 'fs';
+import * as path from 'path';
+import { getUncaughtExceptionLog, getUnhandledRejectionLog } from './utils';
 dotenv.config();
 
 const port = process.env.PORT ?? 4000;
@@ -15,5 +18,29 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(port);
+
+  process.on('uncaughtException', (err, origin) => {
+    const EXCEPTION_LOG_FILE_NAME = 'uncaughtException_log.txt';
+
+    const logFilePath = path.join(
+      __dirname + `/../logs/${EXCEPTION_LOG_FILE_NAME}`,
+    );
+
+    fs.appendFileSync(logFilePath, getUncaughtExceptionLog(err, origin, false));
+
+    fs.writeSync(process.stderr.fd, getUncaughtExceptionLog(err, origin));
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    const EXCEPTION_LOG_FILE_NAME = 'unhandledRejection_log.txt';
+
+    const logFilePath = path.join(
+      __dirname + `/../logs/${EXCEPTION_LOG_FILE_NAME}`,
+    );
+
+    fs.appendFileSync(logFilePath, getUnhandledRejectionLog(reason, promise));
+
+    fs.writeSync(process.stderr.fd, getUnhandledRejectionLog(reason, promise));
+  });
 }
 bootstrap();
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { validate as uuidValidate } from 'uuid';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { entity } from './interfaces';
+import { EOL } from 'os';
 
 export const isValidId = (id: string) => uuidValidate(id);
 
@@ -13,4 +14,17 @@ export const notFoundError = (
 
 export const invalidIdBadRequest = () => {
   throw new HttpException('id is invalid (not uuid)', HttpStatus.BAD_REQUEST);
+};
+
+export const getUncaughtExceptionLog = (err, origin, short = true) => {
+  if (short) {
+    return `${EOL}Date: ${new Date().toUTCString()},${EOL}Caught exception: ${err},${EOL}Exception origin: ${origin}${EOL}`;
+  }
+  return `${EOL}Date: ${new Date().toUTCString()},${EOL}Caught exception: ${err},${EOL}Exception origin: ${origin}${EOL}Stack:${EOL}${JSON.stringify(
+    err.stack,
+  )}${EOL}`;
+};
+
+export const getUnhandledRejectionLog = (reason, promise) => {
+  return `${EOL}Date: ${new Date().toUTCString()},${EOL}Rejection: ${promise},${EOL}Reason: ${reason}${EOL}`;
 };
